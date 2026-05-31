e08c79382fd6f003b08c8d9f03ad5371eb65cf87
HamletHamilton <medelmille@gmail.com>
2026-05-31 20:05:12 +0200

    mindmapmodel class

---
+++ gui/src/main/java/se/su/inlupp/MindMapModel.java
@@ -0,0 +1,49 @@
+package se.su.inlupp;
+import java.util.*;
+public class MindMapModel {
+
+    private Graph<String> graph;
+    private PathFinder<String> pathFinder;
+
+    public MindMapModel() {
+        graph = new ListGraph<>();
+        pathFinder = new DFSPathFinder<>();
+    }
+
+    public void addIdea(String idea) {
+        graph.add(idea);
+    }
+
+    public void removeIdea(String idea) {
+        graph.remove(idea);
+    }
+
+    public void connectIdeas(String from, String to,
+                             String relation, int weight) {
+        graph.connect(from, to, relation, weight);
+    }
+
+    public void disconnectIdeas(String from, String to) {
+        graph.disconnect(from, to);
+    }
+
+    public Path<String> findPath(String from, String to) {
+        return pathFinder.findPath(graph, from, to);
+    }
+
+    public void useDFS() {
+        pathFinder = new DFSPathFinder<>();
+    }
+
+    public void useBFS() {
+        pathFinder = new BFSPathFinder<>();
+    }
+
+    public Set<String> getIdeas() {
+        return graph.getNodes();
+    }
+
+    public Graph<String> getGraph() {
+        return graph;
+    }
+}
